8b49036a5b50ca4517aaa199b4322414b0336c1d
QxQstar <1395294694@qq.com>
2016-10-12 21:32:35 +0800

    babel

---
+++ webpack.es6.config.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Administrator on 2016/10/8.
+ */
+var autoHtml = require('html-webpack-plugin');
+module.exports = {
+    entry:{
+        one:"./app/one.js",
+        two:"./app/two.js"
+    },
+    output:{
+        path:"./build/",
+        filename:"[name].js"
+    },
+    module:{
+        loaders:[
+            {
+                test:/.*\.css$/,
+                loaders:["style","css"],
+                exclude:'./node_modules/'
+            }
+        ]
+    },
+    resolve:{
+        extensions:['','.css','.js','jsx']
+    },
+    plugins:[
+        new autoHtml({
+            'title':"hello",
+            'filename':"class1.html",
+            chunks:['one']
+        })
+    ]
+};
